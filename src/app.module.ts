138088da6aac4351c4292c7f9905db91704c1ba7
AntonyMGJA <114252163+AntonyMGJA@users.noreply.github.com>
2024-06-12 09:41:14 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstadoModule } from './estado/estado.module';
 import { MunicipioModule } from './municipio/municipio.module';
 import { LocalidadModule } from './localidad/localidad.module';
-import { ClienteModule } from './cliente/cliente.module';
+import { ClienteModule } from './modul-client/cliente.module';
 
 @Module({
   imports: [
